79befb9e8c12ed56b1fba1579010fac1e095c2c3
Irechukwu Confidence <irechukwuconfidence@mail.com>
2023-12-25 20:36:21 +0100

    updated

--- model/userModel.js
+++ model/userModel.js
@@ -1,60 +1,72 @@
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    firstname:{
-        type:String,
-        required:[true, "Firstname is Required"]
+    firstname: {
+        type: String,
+        required: [true, "Firstname is Required"]
     },
-    lastname:{
-        type:String,
-        required:[true, "Lastname is Required"]
+    lastname: {
+        type: String,
+        required: [true, "Lastname is Required"]
     },
-    email:{
-        type:String,
-        required:[true, "Email is Required"]
+    email: {
+        type: String,
+        required: [true, "Email is Required"]
     },
-    password:{
-        type:String,
-        required:[true, "Password is Required"]
+    password: {
+        type: String,
+        required: [true, "Password is Required"]
     },
-    profilephoto:{
-        type:String,
+    profilephoto: {
+        type: String,
     },
-    gender:{
-        type:String,
+    gender: {
+        type: String,
     },
-    age:{
-        type:Number,
+    age: {
+        type: Number,
     },
-    phone:{
-        type:Number,
+    phone: {
+        type: Number,
     },
-    isBlocked:{
-        type:Boolean,
-        default:false
+    blocked:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
+    ],
+    lists: [
+        {
+            type: mongoose.Schema.Type.ObjectId,
+            ref: List
+        },
+    ],
+    isBlocked: {
+        type: Boolean,
+        default: false
     },
-    isAdmin:{
-        type:Boolean,
-        default:false
+    isAdmin: {
+        type: Boolean,
+        default: false
     },
-    role:{
-        type:String,
-        enum:["Admin","User","Agent"],
-        default:"User"
+    role: {
+        type: String,
+        enum: ["Admin", "User", "Agent"],
+        default: "User"
     },
-    resetToken:{
-        type:String
+    resetToken: {
+        type: String
     },
-    resetTokenExpiration:{
-        type:Date
+    resetTokenExpiration: {
+        type: Date
     },
 },
-{
-        timestamps:true,
-        toJSON:{virtuals:true}
-    
-}
+    {
+        timestamps: true,
+        toJSON: { virtuals: true }
+
+    }
 );
 
-const User = mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
 export default User
