f1fb4e8f7a708553131a2e4c1c54f89fca55b5c0
Irechukwu Confidence <irechukwuconfidence@mail.com>
2024-01-06 04:11:30 +0100

    app.use added for error handler

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express"
 import dotenv from "dotenv"
 import userRoute from "./routes/userRoutes.js";
 import { database } from "./config/dBconnect.js";
+import globalErrorHandler from "./middlewear/globalErrorHandler.js";
+
 
 dotenv.config();
 const app = express();
@@ -12,5 +14,8 @@ app.use(express.json())
 const PORT = process.env.PORT || 8080
 
 app.use("/api/v1/users", userRoute)
+
+
+app.use(globalErrorHandler);
 
 app.listen(PORT,console.log(`App Started at ${PORT}`))
