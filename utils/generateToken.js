b610ad75fde213fe0cd8c9ceaa7fba56fef07061
Irechukwu Confidence <irechukwuconfidence@mail.com>
2024-01-07 17:55:30 +0100

    updated

---
+++ utils/generateToken.js
@@ -0,0 +1,17 @@
+// import jwt from 'jsonwebtoken'
+
+// const generateToken = id => {
+//     return jwt.sign({id}, process.env.JWT_KEY, {
+//         expiresIn:process.env.TOKEN_EXPIRES
+//     })
+// }
+
+// export default generateToken
+
+import jwt from 'jsonwebtoken'
+
+const generateToken = id =>{
+    return jwt.sign({id}, process.env.JWT_KEY, {expiresIn:process.env.TOKEN_EXPIRES});
+}
+
+export default generateToken;
