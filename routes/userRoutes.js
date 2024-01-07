b4c3038cd4d4b839fc7a530b889d50286ffcd2b7
Irechukwu Confidence <irechukwuconfidence@mail.com>
2024-01-07 17:54:08 +0100

    route for isLogin created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,22 @@
 import express from "express";
 import { 
     createUserCtr, 
+    displayAllController, 
+    profile, 
     userLoginCtrl 
 } from "../controller/userController.js";
+import { isLogin } from "../middlewear/isLogin.js";
 
 const userRoute = express.Router();
 
 //create user
 userRoute.post("/create", createUserCtr);
-userRoute.post("/login", userLoginCtrl)
+//login
+userRoute.post("/login", userLoginCtrl);
+//profile
+userRoute.get("/profile", isLogin, profile)
+//userRoute.get("/:id",isLogin , profile)
+//get users
+userRoute.get("",isLogin,displayAllController);
 
 export default userRoute;
